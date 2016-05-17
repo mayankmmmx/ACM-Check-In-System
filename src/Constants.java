f7335a5e014ba588f55f141d9f032c94b9400562
Mayank Makwana <mayankmmmx@Mayanks-MBP.fios-router.home>
2016-05-17 14:18:45 -0400

    updates global constants

--- src/Constants.java
+++ src/Constants.java
@@ -1,6 +1,12 @@
 /*
  * This class stores all of the global constants used in this application. 
+ * These constants are set on the login page.
  */
 public class Constants {
-
+	
+	public static String userName = ""; //Database login (default is "points")
+	public static String password = ""; //Database password (check Officer wiki for password)
+	public static String databaseName = ""; //Database name (default is "points")
+	public static String tableName = ""; // Table name (default format is points_<semester: fall/string>_<yr: xx>, i.e. points_fall_16)
+	
 }
