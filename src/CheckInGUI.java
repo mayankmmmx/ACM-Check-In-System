1352f5d9175108574d7a6a76ba0b3cabef63dda7
Mayank Makwana <mayankmmmx@Mayanks-MBP.fios-router.home>
2016-05-17 01:18:46 -0400

    Added GUI file that will be later edited to incorporate GUI elements

---
+++ src/CheckInGUI.java
@@ -0,0 +1,13 @@
+/*
+ * This class contains the GUI elements of the application
+ */
+import java.sql.*;
+public class CheckInGUI {
+
+	public static void main(String[] args) throws SQLException {
+		// TODO Auto-generated method stub
+			
+
+	}
+
+}
