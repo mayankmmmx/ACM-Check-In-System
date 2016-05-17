9578d331f3ca67fc3584e7c7b8e1e63f6c63f55e
Mayank Makwana <mayankmmmx@gmail.com>
2016-05-17 14:29:26 -0400

    Added User class that simulates database entry

---
+++ src/User.java
@@ -0,0 +1,55 @@
+/*
+ * This class represents a user entry from the database
+ */
+import java.sql.Timestamp;
+
+public class User {
+	
+	private String cardID;
+	private String accessID;
+	private int points;
+	private Timestamp time;
+	
+	/*
+	 * Default Constructor taking in cardID, accessID, points, and last checked in
+	 */
+	public User(String _cardID, String _accessID, int _points, Timestamp _time)
+	{
+		cardID = _cardID;
+		accessID = _accessID;
+		points = _points;
+		time = _time;
+	}
+	
+	/*
+	 * Gets Card ID
+	 */
+	public String getCardID()
+	{
+		return cardID;
+	}
+	
+	/*
+	 * Gets Access ID
+	 */
+	public String getAccessID()
+	{
+		return accessID;
+	}
+	
+	/*
+	 * Gets Points
+	 */
+	public int getPoints()
+	{
+		return points;
+	}
+	
+	/*
+	 * Gets last timestamp
+	 */
+	public Timestamp getTimeStamp()
+	{
+		return time;
+	}
+}
